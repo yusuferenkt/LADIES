6629be973ed0424855e800b2b39956f02bc39d4b
melis <131777538+melistunc@users.noreply.github.com>
2024-05-15 17:06:20 +0300

    Delete Frontend directory

--- Frontend/favorites.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  var favoritesList = document.getElementById("favorites-list");
-  favorites.forEach(function (productName) {
-    var listItem = document.createElement("li");
-    listItem.textContent = productName;
-    favoritesList.appendChild(listItem);
-  });
-});
--- Frontend/script.js
+++
@@ -1,7 +0,0 @@
-function addToFavorites(productName) {
-  var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  if (!favorites.includes(productName)) {
-    favorites.push(productName);
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-  }
-}
